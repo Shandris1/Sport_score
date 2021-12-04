7df987122026c82baee21539e341bda102ae787f
Shandris <Shandris@users.noreply.github.com>
2021-12-04 22:14:30 +0000

    Wrote tests for the data structure + created pom file

--- src/worldCup/gameTest.java
+++ src/worldCup/gameTest.java
@@ -2,38 +2,57 @@ package worldCup;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class gameTest {
-
+	private game game1 = new game("Mexico","Brazil");
+	private game game2 = new game("Russia","China"); 
+	
+	
+	@BeforeEach
+	public void init() {
+		}
+	
 	@Test
 	void testGame() {
-		fail("Not yet implemented");
+		assertTrue(game1.displaySides()[0].equals("Mexico"));
+		assertTrue(game1.displaySides()[1].equals("Brazil"));
 	}
 
 	@Test
 	void testIncreaseHomeScore() {
-		fail("Not yet implemented");
+		assertTrue(game1.displayScore()[0]==0);
+		game1.increaseHomeScore(1);
+		assertTrue(game1.displayScore()[0]==1);
 	}
 
 	@Test
 	void testIncreaseAwayScore() {
-		fail("Not yet implemented");
+		assertTrue(game1.displayScore()[1]==0);
+		game1.increaseAwayScore(1);
+		assertTrue(game1.displayScore()[1]==1);
 	}
 
 	@Test
 	void testDisplayScore() {
-		fail("Not yet implemented");
+		assertTrue(game1.displayScore()[1]==0);
 	}
 
 	@Test
 	void testDisplaySides() {
-		fail("Not yet implemented");
+		assertTrue(game1.displaySides()[0].equals("Mexico"));
 	}
 
 	@Test
 	void testCombinedScore() {
-		fail("Not yet implemented");
+		assertTrue(game1.combinedScore()==0);
+		game1.increaseHomeScore(1);
+		assertTrue(game1.combinedScore()==1);
+		game1.increaseAwayScore(1);
+		assertTrue(game1.combinedScore()==2);
 	}
 
 }
